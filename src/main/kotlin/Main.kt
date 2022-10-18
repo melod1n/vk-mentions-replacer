8ca298116e4a63732d60e8d1bb0086cd475c1b49
Danil Nikolaev <lischenkodev@gmail.com>
2022-10-18 23:48:13 +0300

    support for ids

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,42 +1,44 @@
 fun main(args: Array<String>) {
-    var string = readLine() ?: ""
+    var newMessageText = readLine() ?: ""
 
-    val indexes = mutableListOf<Pair<Int, Int>>()
+    val idsIndexes = mutableListOf<Triple<Int, Int, Int>>()
     val mentions = hashMapOf<String, String>()
 
     var startFrom = 0
 
     while (true) {
-        val leftBracketIndex = string.indexOf('[', startFrom)
-        val verticalLineIndex = string.indexOf('|', startFrom)
-        val rightBracketIndex = string.indexOf(']', startFrom)
+        val leftBracketIndex = newMessageText.indexOf('[', startFrom)
+        val verticalLineIndex = newMessageText.indexOf('|', startFrom)
+        val rightBracketIndex = newMessageText.indexOf(']', startFrom)
 
         if (leftBracketIndex == -1 ||
             verticalLineIndex == -1 ||
             rightBracketIndex == -1
         ) break
 
-        val id = string.substring(leftBracketIndex + 1, verticalLineIndex)
+        val idPart = newMessageText.substring(leftBracketIndex + 1, verticalLineIndex)
 
-        if (!id.matches(Regex("^id(\\d+)\$")) || rightBracketIndex - verticalLineIndex < 2) {
+        val actualId = idPart.substring(2, idPart.length).toIntOrNull() ?: -1
+
+        if (!idPart.matches(Regex("^id(\\d+)\$")) || rightBracketIndex - verticalLineIndex < 2) {
             break
         }
 
-        val text = string.substring(verticalLineIndex + 1, rightBracketIndex)
+        val text = newMessageText.substring(verticalLineIndex + 1, rightBracketIndex)
 
-        val str = "[$id|$text]"
+        val str = "[$idPart|$text]"
 
         mentions[str] = text
 
-        indexes += leftBracketIndex to leftBracketIndex + text.length
+        idsIndexes += Triple(actualId, leftBracketIndex, leftBracketIndex + text.length)
 
         startFrom = rightBracketIndex + 1
     }
 
     mentions.forEach {
-        string = string.replace(it.key, it.value)
+        newMessageText = newMessageText.replace(it.key, it.value)
     }
 
     println()
-    println("final string: $string")
+    println("final string: $newMessageText")
 }
