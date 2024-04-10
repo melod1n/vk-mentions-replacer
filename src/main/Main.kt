1d5a50d75bb7749d5ed17244feda4358ed6cecb7
Danil Nikolaev <lischenkodev@gmail.com>
2024-04-11 01:10:26 +0300

    update algorithm

---
+++ src/main/Main.kt
@@ -0,0 +1,27 @@
+fun main() {
+    val input = readlnOrNull() ?: ""
+
+    val regex = """\[(id|club)(\d+)\|([^]]+)]""".toRegex()
+
+    val indexes = mutableListOf<MentionIndex>()
+
+    val result = input.replace(regex) { result ->
+        val group = requireNotNull(result.groups[2])
+        val id = group.value.toIntOrNull() ?: -1
+        val indexRange = group.range
+
+        indexes += MentionIndex(
+            id = id,
+            indexRange = indexRange
+        )
+
+        result.groups[3]?.value ?: ""
+    }
+
+    println("Результат: $result")
+}
+
+private data class MentionIndex(
+    val id: Int,
+    val indexRange: IntRange
+)
